e04bad514aada1bf0c3d1b352b01e4492d1121b4
zxq <461890276@qq.com>
2020-04-20 21:48:46 +0800

    'user-api'

--- zxq-test/routes/user.js
+++ zxq-test/routes/user.js
@@ -20,20 +20,57 @@ db.connect((err) => {
   }
 })
 
-// 登录验证
+// 登录验证,未注册就注册且自动登录
 router.post('/login', function (req, res) {
-  console.log(req.body);
   let params = req.body;
 　let username = params.username;
 　let password = params.password;
   let sql = `SELECT * FROM user_info WHERE username = '${username}'`
-  db.query(sql, (err, result) => {
+  var psw = '';
+  db.query(sql, (err, result0) => {
     if (err) {
-      console.log(err)
-    } else {
-      resultData = result[0]
-      if (resultData.password === password) {
-        console.log("success!")
+      res.json({
+        code: 1,
+        msg: ' error',
+        data: ''
+      })
+      return false;
+    } else if (result0.length === 0) {    // 未注册
+      if (!username || !password) {
+        res.json({
+          code: 1,
+          msg: 'register is error',
+          data: ''
+        })
+        return false;
+      }
+      let sql = `INSERT into user_info (username,PASSWORD) VALUES ("'${username}'", "'${password}'")`
+      db.query(sql, (err, result1) => {
+        if (err) {
+          console.log(err)
+          res.json({
+            code: 1,
+            msg: 'Register user error',
+            data: ''
+          })
+          return false;
+        } else {
+          const token = jwt.sign({
+            username,
+            password,
+            'time': Date.now(),
+          },sercet,{expiresIn:60*60});
+          res.json({
+            code: 0,
+            msg: 'success',
+            data: token
+          });
+        } 
+      })
+    } else {          // 已注册，验证登录
+      resultData = result0[0]
+      psw = resultData.password
+      if (psw === password) {
         var data = '';
         req.on('data', function (chunk) {
           data += chunk;
@@ -52,8 +89,131 @@ router.post('/login', function (req, res) {
           data: token
         });
       } else {
+        res.json({
+          code: 1,
+          message: "Login error",
+          data: ''
+        })
         return false
       }
+    }
+  })
+})
+
+// 修改头像
+router.post('/updateAvatar', function (req, res) {
+  let params = req.body;
+  let headers = req.headers;
+  console.log(headers.token);
+  let tokenStr = headers.token || 'error' ;
+　let avatar = params.avatar_url;
+  let tokenData = jwt.verify(tokenStr, sercet);
+  console.log(tokenData)
+  console.log(avatar)
+  var username = tokenData.username
+  var password = tokenData.password
+  let sql = `SELECT * FROM user_info WHERE username = '${username}'`
+  db.query(sql, (err, result_avatar) => {
+    if (err) {
+      console.log(err)
+      res.json({
+        code: 1,
+        msg: 'token.username is wrong',
+        data: ''
+      })
+      return false;
+    } else if (password === result_avatar[0].password) {
+      if (avatar) {
+        let update_sql = `update user_info set avatar = '${avatar}' where username = '${username}'`;
+        db.query(update_sql, (err, result_avatarurl) => {
+          if (err) {
+            console.log(err)
+            res.json({
+              code: 1,
+              msg: 'update failed',
+              data: ''
+            })
+            return false;
+          } else {
+            res.json({
+              code: 0,
+              msg: 'update avatar success',
+              data: ''
+            })
+          }
+        })
+      } else {
+        res.json({
+          code: 1,
+          msg: 'avatar is wrong',
+          data: ''
+        })
+      }
+    } else {
+      res.json({
+        code: 1,
+        msg: 'token.password is wrong',
+        data: ''
+      })
+    }
+  })
+})
+
+// 修改密码
+router.post('/updatePsw', function (req, res) {
+  let params = req.body;
+  let headers = req.headers;
+  console.log(headers.token);
+  let tokenStr = headers.token || 'error' ;
+　let newpsw = params.newPassword;
+  let tokenData = jwt.verify(tokenStr, sercet);
+  console.log(tokenData)
+  console.log(newpsw)
+  var username = tokenData.username
+  var password = tokenData.password
+  let sql = `SELECT * FROM user_info WHERE username = '${username}'`
+  db.query(sql, (err, result_newpsw) => {
+    if (err) {
+      console.log(err)
+      res.json({
+        code: 1,
+        msg: 'token.username is wrong',
+        data: ''
+      })
+      return false;
+    } else if (password === result_newpsw[0].password) {
+      if (newpsw) {
+        let update_sql = `update user_info set password = '${newpsw}' where username = '${username}'`;
+        db.query(update_sql, (err, result_newpsw_res) => {
+          if (err) {
+            console.log(err)
+            res.json({
+              code: 1,
+              msg: 'update failed',
+              data: ''
+            })
+            return false;
+          } else {
+            res.json({
+              code: 0,
+              msg: 'update password success',
+              data: ''
+            })
+          }
+        })
+      } else {
+        res.json({
+          code: 1,
+          msg: 'new_password is wrong',
+          data: ''
+        })
+      }
+    } else {
+      res.json({
+        code: 1,
+        msg: 'token.password is wrong',
+        data: ''
+      })
     }
   })
 })
